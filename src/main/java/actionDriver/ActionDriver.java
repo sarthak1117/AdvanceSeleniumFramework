a359a4b53ff9aeb589b499a7b655311b9e130c53
sarthak1117 <sarthak738@gmail.com>
2026-02-15 01:24:14 +0530

    Action driver update

--- src/main/java/actionDriver/ActionDriver.java
+++ src/main/java/actionDriver/ActionDriver.java
@@ -3,6 +3,7 @@ package actionDriver;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -18,7 +19,7 @@ public class ActionDriver {
         this.wait = new WebDriverWait(driver, Duration.ofSeconds(30));
 
     }
-
+    //method to click on the element
     public void click(By by){
         try{
         waitForElementToBeClickable(by);
@@ -28,7 +29,7 @@ public class ActionDriver {
             System.out.println("Unable to click on the element:" + e.getMessage());
         }
     }
-
+    //method to enter text in the element
     public void enterText(By by, String text){
         try{
             waitForElementToBeVisible(by);
@@ -37,6 +38,73 @@ public class ActionDriver {
         }
         catch(Exception e){
             System.out.println("Unable to enter text in the element:" + e.getMessage());
+        }        
+    }
+
+    //get text from the element
+    public String getText(By by){
+        try{
+            waitForElementToBeVisible(by);
+            return driver.findElement(by).getText();
+        }
+        catch(Exception e){
+            System.out.println("Unable to get text from the element:" + e.getMessage());
+            return null;
+        }
+        
+    }
+    
+    //compare the expected text with the actual text
+    public void compareText(By by, String ExpectedText){
+        try {
+            waitForElementToBeVisible(by);
+        String actualText = getText(by);
+        if(actualText !=null && actualText.equals(ExpectedText)){
+            System.out.println("Text is matching: " + actualText);
+        }
+        else{
+            System.out.println("Text is not matching. Expected: " + ExpectedText + ", Actual: " + actualText);
+        }
+            
+        } catch (Exception e) {
+            System.out.println("Error comparing text: " + e.getMessage());
+        }
+        
+    }
+
+    public boolean isDisplayed(By by){
+        try {
+            waitForElementToBeVisible(by);
+            boolean isDisplayed = driver.findElement(by).isDisplayed();
+            if(isDisplayed){
+                System.out.println("Element is displayed: " + by.toString());
+                return isDisplayed;
+            }
+        else return isDisplayed;
+            
+        } catch (Exception e) {
+            System.out.println("Element is not displayed: " + e.getMessage());
+            return false;
+        }
+    }
+
+    public void waitForPageToLoad(){ 
+        try { 
+            wait.until(webDriver -> ((JavascriptExecutor) webDriver).executeScript("return document.readyState").equals("complete")); 
+        } 
+        catch (Exception e) { 
+            System.out.println("Error waiting for page to load: " + e.getMessage()); 
+        } 
+    }   
+
+    public void scrollToElement(By by){
+        try {
+            JavascriptExecutor js = (JavascriptExecutor) driver;
+            waitForElementToBeVisible(by);
+            js.executeScript("arguments[0].scrollIntoView(true);", driver.findElement(by));
+            
+        } catch (Exception e) {
+            System.out.println("Error scrolling to element: " + e.getMessage());
         }        
     }
 
